0bb365a3b534724eece1ecbe2408d2e9393fafee
ioana <ioanav3796@gmail.com>
2019-10-19 23:42:52 +0200

    Updates

---
+++ precache-manifest.cb397c8f5e491df8cb8f01b6de7fe8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf39e5f695b41816d15610ecabb3fe0",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "14f3542dec01428e3621",
+    "url": "/react-weather-app/static/css/main.8834e72e.chunk.css"
+  },
+  {
+    "revision": "8b9a7a1ef7826ffbfe39",
+    "url": "/react-weather-app/static/js/2.878de447.chunk.js"
+  },
+  {
+    "revision": "14f3542dec01428e3621",
+    "url": "/react-weather-app/static/js/main.10374e6e.chunk.js"
+  },
+  {
+    "revision": "475c08e222e88c2c2f2c",
+    "url": "/react-weather-app/static/js/runtime-main.78a55ada.js"
+  },
+  {
+    "revision": "e86a85ee2094c66805a69c18d8f1c0b3",
+    "url": "/react-weather-app/static/media/background-2.e86a85ee.jpg"
+  }
+]);
